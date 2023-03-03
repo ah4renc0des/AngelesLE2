00b5f27b8870fbec1d1fb794611d5dd3f1baffe3
ah4renc0des <angelesalexandra@gmail.com>
2023-03-03 22:27:22 +0800

    Add project files.

---
+++ BlogDataLibrary/Database/SqlData.cs
@@ -0,0 +1,20 @@
+﻿using BlogDataLibrary.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogDataLibrary.Database
+{
+    public class SqlData
+    {
+        private IsqlDataAccess _db;
+        private const string connectionStringName = "SqlDb";
+
+        public SqlData(IsqlDataAccess db)
+        {
+            _db = db;
+        }
+    }
+}
